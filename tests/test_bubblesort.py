a9eedaebe7998cc29fcb7aee6d2bf8e7b7f922b9
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-01 16:28:15 +0300

    Add extra lines

--- tests/test_bubblesort.py
+++ tests/test_bubblesort.py
@@ -3,11 +3,14 @@ from src.sorting.bubblesort import bubblesort
 def testEmpty():
     assert bubblesort([]) == []
 
+
 def testOneElm():
     assert bubblesort([1]) == [1]
 
+
 def testNegativeElm():
     assert bubblesort([-1, -2, -3]) == [-3, -2, -1]
+
 
 def testLargeElm():
     assert bubblesort([1000000000, 2000000000]) == [1000000000, 2000000000]
--- tests/test_heapsort.py
+++ tests/test_heapsort.py
@@ -3,11 +3,14 @@ from src.sorting.heapsort import heapsort
 def testEmpty():
     assert heapsort([]) == []
 
+
 def testOneElm():
     assert heapsort([1]) == [1]
 
+
 def testNegativeElm():
     assert heapsort([-1, -2, -3]) == [-3, -2, -1]
+
 
 def testLargeElm():
     assert heapsort([1000000000, 2000000000]) == [1000000000, 2000000000]
