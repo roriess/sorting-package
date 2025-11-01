71c02ff03681110e2c783336c02cf588838e3cdb
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-01 16:24:12 +0300

    Add tests for bubble sort

---
+++ tests/test_bubblesort.py
@@ -0,0 +1,17 @@
+from src.sorting.bubblesort import bubblesort
+
+def testEmpty():
+    assert bubblesort([]) == []
+
+def testOneElm():
+    assert bubblesort([1]) == [1]
+
+def testNegativeElm():
+    assert bubblesort([-1, -2, -3]) == [-3, -2, -1]
+
+def testLargeElm():
+    assert bubblesort([1000000000, 2000000000]) == [1000000000, 2000000000]
+
+
+def testNone():
+    assert bubblesort(None) is None
