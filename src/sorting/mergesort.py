9856a4499700b04326372a5fd5e70b35729f6ee5
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-01 16:09:10 +0300

    Add 'return arr'

--- src/sorting/mergesort.py
+++ src/sorting/mergesort.py
@@ -1,13 +1,10 @@
 def mergesort(arr):
     for i in range(len(arr)):
         j = i - 1
-        while j >= 0 and arr[j] > arr[i]:
+        key = arr[i]
+        while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
-        arr[j + 1] = arr[i]
+        arr[j + 1] = key
 
-
-arr = [1, 56, 34, 23, 78, 90, 12, -14256, -13]
-mergesort(arr)
-
-print(arr)
+    return arr
