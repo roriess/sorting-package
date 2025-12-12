69938822fecc70150d22627a8496c4b913c18549
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-12-12 20:37:34 +0300

    changed sorting in mergesort.py

--- src/sorting/mergesort.py
+++ src/sorting/mergesort.py
@@ -1,14 +1,22 @@
-def mergesort(arr):
-    if arr is None: return None
+def merge_sort(arr):
+    if len(arr) <= 1: 
+        return arr
+    
+    mid = len(arr) // 2
 
-    for i in range(len(arr)):
-        j = i - 1
-        key = arr[i]
+    first_part = arr[:mid]
+    second_part = arr[mid:]
 
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
+    first_part = merge_sort(first_part)
+    second_part = merge_sort(second_part)
 
-        arr[j + 1] = key
+    return merge(first_part, second_part)
 
-    return arr
+def merge(first, second):
+    merged = []
+    while first and second:
+        if first[0] > second[0]:
+            merged.append(second.pop(0))
+        else:
+            merged.append(first.pop(0))
+    merged.extend(first or second)
